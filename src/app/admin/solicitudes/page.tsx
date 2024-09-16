acdce94107416d2d92b347d85a401ac0891d8109
Ignacio Sekhard <nachostifler@gmail.com>
2024-09-16 17:07:10 -0300

    incorporacion de la página

---
+++ src/app/admin/solicitudes/page.tsx
@@ -0,0 +1,10 @@
+import SolicitudesAdmin from '@/components/SolicitudesAdmin'
+
+//Definimos el componente AdminSolicitudes y le asignamos el componente SolicitudesAdmin
+export default function AdminSolicitudes() {
+  return (
+    <>
+      <SolicitudesAdmin />
+    </>
+  );
+}
