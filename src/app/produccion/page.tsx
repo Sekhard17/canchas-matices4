2b43866fc9e08fba8fc53f475104fc5c626c9ffe
Ignacio Sekhard <nachostifler@gmail.com>
2024-09-16 19:26:04 -0300

    mejoras visuales

--- src/app/produccion/page.tsx
+++ src/app/produccion/page.tsx
@@ -3,9 +3,7 @@ import Produccion from "@/components/Produccion";
 export default function Product() {
   return (
     <>
-      <div className="container mx-auto p-4">
         <Produccion />
-      </div>
     </>
   );
 }
