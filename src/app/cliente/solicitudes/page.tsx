8171d563999ad6068ed07251843c07961c73da46
Ignacio Sekhard <nachostifler@gmail.com>
2024-09-16 16:45:27 -0300

    Cambios

--- src/app/cliente/solicitudes/page.tsx
+++ src/app/cliente/solicitudes/page.tsx
@@ -1,9 +1,10 @@
 import Solicitudes from "@/components/Solicitudes";
 
+//Definimos el componente ClienteDash y le asignamos el componente Solicitudes
 export default function ClienteDash() {
   return (
     <>
-      
+        
         <Solicitudes />
     </>
   );
