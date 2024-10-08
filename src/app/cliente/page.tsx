0ac7382d9f61988122ffa4a924df8a83c2eb7961
Ignacio Sekhard <nachostifler@gmail.com>
2024-10-08 13:30:49 -0300

    Cambios en ClienteDashboard.

--- src/app/cliente/page.tsx
+++ src/app/cliente/page.tsx
@@ -1,13 +1,9 @@
 import ClienteDashboard from "@/components/ClienteDashboard";
-import Navbar from "@/components/Navbar";
 
 export default function ClienteDash() {
   return (
     <>
-      <Navbar />
-      <div className="container mx-auto p-4">
         <ClienteDashboard />
-      </div>
     </>
   );
 }
--- src/components/ClienteDashboard.tsx
+++ src/components/ClienteDashboard.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import {jwtDecode} from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode'
 import { motion, AnimatePresence } from 'framer-motion'
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
